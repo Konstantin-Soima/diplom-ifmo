c6325efa69b7a443e95f083bd3443ca4e8d3fed4
Константин <soima_ka@spbexp.ru>
2020-07-09 15:45:40 +0300

    JavaFX инициализируется, криво, события  выбора категорий

--- src/main/java/worker/WorkWindowController.java
+++ src/main/java/worker/WorkWindowController.java
@@ -1,39 +1,67 @@
 package worker;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import common.City;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.List;
 
 public class WorkWindowController  {
 
 
     @FXML
-    private ListView<String> cityList = new ListView<>();
+    private ListView<City> cityList = new ListView<>();
 
-    private ObservableList<String> observableList = FXCollections.observableArrayList("Санкт-Петербург", "Москва");
+    private ObservableList<City> observableList ;
 
 
 
-    public void initialize(){
+    public void initialize() throws IOException {
         //TODO: форма ввода, выбор групп, ключевые слова
-
+        //города
+        String cityListJson = Jsoup.connect("http://188.242.232.214:8080/city").ignoreContentType(true).execute().body();
+        System.out.println(cityListJson);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<City> listCity = objectMapper.readValue(cityListJson, new TypeReference<List<City>>(){});
+        System.out.println(listCity.size());
+        observableList = FXCollections.observableArrayList(listCity);
         cityList.setItems(observableList);
-        System.out.println(cityList.getItems().size());
+        cityList.setCellFactory(param -> new ListCell<City>() {
+            @Override
+            protected void updateItem(City p, boolean empty){
+                super.updateItem(p, empty);
+                if(empty || p == null || p.getName() == null){
+                    setText("");
+                }
+                else{
+                    setText(p.getName()); //вывожу просто имя города
+                }
+            }
+        });
+        //выбор файла при выборе
+        cityList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<City>() {
+            @Override
+            public void changed(ObservableValue<? extends City> observable, City oldValue, City newValue) {
+                // Получаем список категорий
+                System.out.println("Selected item: " + newValue);
 
-        //Document document = Jsoup.connect("http://188.242.232.214:8080/city").get();
-        //проверка
-        String cityListJson ="[{\"id\":1,\"name\":\"Санкт-Петербург\"},{\"id\":2,\"name\":\"Санкт-Петербург и ЛО\"},{\"id\":3,\"name\":\"Москва\"}]";
-
+            }
+        });
 
     }
     void initData() {
